317a8bafbe249efa9a82c3bf5ad43bf9d378316d
Angelo Zurita <nzurita@fiec.espol.edu.ec>
2023-04-27 16:10:56 -0500

    ok

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+
 
 app = Dash(__name__)
 
@@ -28,8 +28,8 @@ def update_figure(selected_year):
     filtered_df = df[df.year == selected_year]
 
     fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
-                     size="pop", color="continent", hover_name="country",
-                     log_x=True, size_max=55)
+                    size="pop", color="continent", hover_name="country",
+                    log_x=True, size_max=55)
 
     fig.update_layout(transition_duration=500)
 
