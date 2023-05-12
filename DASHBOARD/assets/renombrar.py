845b9bc76732d7a978d3bc81b06bbd3b2d45ef37
aszurita <nzuritag@fiec.espol.edu.ec>
2023-05-12 09:47:45 -0500

    ok

--- DASHBOARD/assets/renombrar.py
+++ DASHBOARD/assets/renombrar.py
@@ -10,12 +10,17 @@ paises=['Ecuador','Argentina','Australia', 'Wales', 'Morocco', 'Qatar', 'Senegal
     'Korea Republic']
 #['Ecuador','Argentina','Wales','Uruguay'] paises ya hechos
 #['Spain','Portugal'] 
+#['Mexico','Korea Republic','Qatar','Brazil','Japan']
 df_img_codes = pd.read_csv('/Users/angelozurita/Repositorios_GitHub/Group_Slytheryn/Data_worldCup/Images_country.csv',sep=';')
-for pais in  ['Mexico','Korea Republic','Qatar','Brazil','Japan']:
+for pais in  paises:
     df_f = df_img_codes[df_img_codes['PAIS']==pais]
     for actual,nuevo in zip(df_f.ACTUAL,df_f.NUEVO):
-        file_oldname = os.path.join(f"/Users/angelozurita/Repositorios_GitHub/Group_Slytheryn/DASHBOARD/assets/Images/{pais}",actual )
-        file_newname_newfile = os.path.join(f"/Users/angelozurita/Repositorios_GitHub/Group_Slytheryn/DASHBOARD/assets/Images/{pais}",nuevo)
-        os.rename(file_oldname, file_newname_newfile)
+        try :
+            file_oldname = os.path.join(f"/Users/angelozurita/Repositorios_GitHub/Group_Slytheryn/DASHBOARD/assets/Images/{pais}",actual )
+            file_newname_newfile = os.path.join(f"/Users/angelozurita/Repositorios_GitHub/Group_Slytheryn/DASHBOARD/assets/Images/{pais}",nuevo)
+            os.rename(file_oldname, file_newname_newfile)
+        except (FileNotFoundError):
+            continue
+
 
         
