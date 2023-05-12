b75774d5f34dc00baac0f26340db74950c2ad076
aszurita <nzuritag@fiec.espol.edu.ec>
2023-05-12 09:09:11 -0500

    ok

--- DASHBOARD/index.py
+++ DASHBOARD/index.py
@@ -31,7 +31,7 @@ navbar = dbc.Navbar(
                 dbc.Row(
                     [
                         dbc.Col(html.Img(src="assets/logo_espol.png", height="30px")),
-                        dbc.Col(dbc.NavbarBrand("SLYTHERYN SOCCER DASHBOARD",)),
+                        dbc.Col(dbc.NavbarBrand("FIFA WORLD CUPS",)),
                     ],
                     align='center',
                     justify='center'
@@ -86,6 +86,61 @@ def display_page(pathname):
         return FIFA_WORLD_CUP_2022.layout
     else:
         return home.layout
+   
+
+html.Nav(class="navbar navbar-expand-lg navbar-dark bg-primary")
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Separated link</a>
+          </div>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-sm-2" type="search" placeholder="Search">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run_server(host='127.0.0.1', debug=True)
--- DASHBOARD/pages/home.py
+++ DASHBOARD/pages/home.py
@@ -13,48 +13,9 @@ from app import app
 layout = html.Div([
     html.Div([
         html.Div([
-            html.Div(html.H1("Welcome to the Slytheryn Soccer dashboard", className="title",)
+            html.Div([
+                html.H1("Welcome to the Slytheryn Soccer dashboard", className="title",)]
                     , className="Container_text_frase",style={'margin-bottom':'4vw'},)
         ]),
-        html.Div([
-            html.Div([
-                html.Div([html.H5(children=' “El día que no disfrute en el campo, voy a dejar el futbol” ')], className="text_frase")
-                ],className='Container_text_frase'),
-            html.Div([
-                html.Div([html.Img(src='https://emprendedor.com/wp-content/uploads/2022/12/lionel-messi-copa-del-mundo-3.jpg',alt='Messi',)],className='container_img'),
-                html.Div([html.Img(src='https://pbs.twimg.com/media/FFnsNEmXwAcdSi-.jpg:large',alt='Messi2')],className='container_img'),
-                html.Div([html.Img(src='https://studiofutbol.com.ec/wp-content/uploads/2023/05/messi-laureus.jpg',alt='Messi3')],className='container_img'),
-                ],className='container_div_img'),
-            html.Div([
-                html.Div([html.H5(children=' “Al final, cuando se termine todo esto, ¿qué te llevas? Mi intención es que, cuando me retire, se me recuerde por ser buen tipo” - Leonel Messi ')], className="text_frase")
-                ],className='Container_text_frase')
-        ],className='frases_jugador'),
-        html.Div([
-            html.Div([
-                html.Div([html.H5(children=' “Cuando la gente tiene éxito, es gracias al sacrificio. La suerte no tiene nada que ver con el éxito” ')], className="text_frase")
-                ],className='Container_text_frase'),
-            html.Div([
-                html.Div([html.Img(src='https://i.ytimg.com/vi/yv_JvjGdi34/maxresdefault.jpg',alt='Messi',)],className='container_img'),
-                html.Div([html.Img(src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Maradona-Mundial_86_con_la_copa.JPG/1200px-Maradona-Mundial_86_con_la_copa.JPG',alt='Messi2')],className='container_img'),
-                html.Div([html.Img(src='https://www.mundodeportivo.com/us/files/article_main_microformat/files/fp/uploads/2022/11/18/6377b9477bb99.r_d.369-213-0.png',alt='Messi3')],className='container_img'),
-                ],className='container_div_img'),
-            html.Div([
-                html.Div([html.H5(children=' “Estoy arrepentido del 99 % de todo lo que hice en mi vida; pero el 1% que es el fútbol salva el resto” - Diego Maradona')], className="text_frase")
-                ],className='Container_text_frase')
-        ],className='frases_jugador'),
-         html.Div([
-            html.Div([
-                html.Div([html.H5(children=' “La gente ve a los futbolistas como seres diferentes, como si fuéramos intocables, como si nunca nos pasara nada, pero somos personas.” ')], className="text_frase")
-                ],className='Container_text_frase'),
-            html.Div([
-                html.Div([html.Img(src='https://d3h7g948tee6ho.cloudfront.net/wp-content/uploads/2018/04/iniesta-copas.jpg',alt='Messi',)],className='container_img'),
-                html.Div([html.Img(src='https://www.sportzcraazy.com/wp-content/uploads/2019/09/Andres-Iniesta-bio-e1567665343886.jpg',alt='Messi2')],className='container_img'),
-                html.Div([html.Img(src='https://cloudfront-us-east-1.images.arcpublishing.com/eluniverso/ZPO4LBYDOBGQZER4GGWFEEY6RE.jpg',alt='Messi3')],className='container_img'),
-                ],className='container_div_img'),
-            html.Div([
-                html.Div([html.H5(children='“Juego para ser feliz, no para ganar nada” - Andrés Iniesta')], className="text_frase")
-                ],className='Container_text_frase')
-        ],className='frases_jugador')
-    ],className='Container_frases_img')
-
+    ])
 ])
