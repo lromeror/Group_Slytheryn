4ef9695311981c9d1aa5a18c14ed0c36e54dc005
aszurita <nzuritag@fiec.espol.edu.ec>
2023-05-27 15:40:36 -0500

    ok

--- DASHBOARD/pages/Cameroon.py
+++ DASHBOARD/pages/Cameroon.py
@@ -90,7 +90,17 @@ def cartas_jugador(name,cod_img,posicion):
     html.Div([html.Img(src=os.path.join(folder_img_seleccion,cod_img),className='img_team')],className='')
     ],className='button col d-flex align-items-md-stretch ')],className='carta_player_button')
     return card
-
+def cartas_jugador_polonia(name,cod_img,posicion):
+    card = html.Div([
+    html.Button([
+    html.Div(
+        [html.P(posicion)],className='text_pos'
+    ),
+    html.Div([html.Img(src=os.path.join(folder_img_players,name+'.png'),className='img_player')],className=''),
+    html.Div([html.P(name)],className='text_name_po'),
+    html.Div([html.Img(src=os.path.join(folder_img_seleccion,cod_img),className='img_team')],className='')
+    ],className='button col d-flex align-items-md-stretch ')],className='carta_player_button')
+    return card
 layout = html.Div([
     html.Div(
     [navbar]),
@@ -103,9 +113,9 @@ layout = html.Div([
     ],className='container_selec'),
     html.Div([
         cartas_jugador(players.Jugador[0],cod_img,players.Posición[0]),
-        cartas_jugador(players.Jugador[1],cod_img,players.Posición[1]),
+        cartas_jugador_polonia(players.Jugador[1],cod_img,players.Posición[1]),
         cartas_jugador(players.Jugador[2],cod_img,players.Posición[2]),
-        cartas_jugador(players.Jugador[3],cod_img,players.Posición[3]),
+        cartas_jugador_polonia(players.Jugador[3],cod_img,players.Posición[3]),
         cartas_jugador(players.Jugador[4],cod_img,players.Posición[4]),
         cartas_jugador(players.Jugador[5],cod_img,players.Posición[5]),
         cartas_jugador(players.Jugador[6],cod_img,players.Posición[6]),
@@ -119,15 +129,15 @@ layout = html.Div([
         cartas_jugador(players.Jugador[14],cod_img,players.Posición[14]),
         cartas_jugador(players.Jugador[15],cod_img,players.Posición[15]),
         cartas_jugador(players.Jugador[16],cod_img,players.Posición[16]),
-        cartas_jugador(players.Jugador[17],cod_img,players.Posición[17]),
+        cartas_jugador_polonia(players.Jugador[17],cod_img,players.Posición[17]),
         cartas_jugador(players.Jugador[18],cod_img,players.Posición[18]),
         cartas_jugador(players.Jugador[19],cod_img,players.Posición[19]),
         cartas_jugador(players.Jugador[20],cod_img,players.Posición[20]),
         cartas_jugador(players.Jugador[21],cod_img,players.Posición[21]),
-        cartas_jugador(players.Jugador[22],cod_img,players.Posición[22]),
+        cartas_jugador_polonia(players.Jugador[22],cod_img,players.Posición[22]),
         cartas_jugador(players.Jugador[23],cod_img,players.Posición[23]),
         cartas_jugador(players.Jugador[24],cod_img,players.Posición[24]),
-        cartas_jugador(players.Jugador[25],cod_img,players.Posición[25]),
+        cartas_jugador_polonia(players.Jugador[25],cod_img,players.Posición[25]),
         cartas_jugador(players.Jugador[26],cod_img,players.Posición[26]),
         ],className='row container_plantilla')
 ])
