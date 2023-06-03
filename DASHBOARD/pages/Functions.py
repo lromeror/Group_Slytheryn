0cc55b13896f58023feb7a590b3f0f7f639b60d7
aszurita <nzuritag@fiec.espol.edu.ec>
2023-06-02 21:49:43 -0500

    ok

--- DASHBOARD/pages/Functions.py
+++ DASHBOARD/pages/Functions.py
@@ -3,7 +3,7 @@ from dash import Dash, html
 import os 
 
 def country_flag(IMAGES_DIR,Id_team):
-    country_flag = dbc.Col([html.Img(src=os.path.join(IMAGES_DIR,Id_team))],className='col-lg-6 col-md-6 col-sm-6 col-xs-12')
+    country_flag = dbc.Col([html.Img(src=os.path.join(IMAGES_DIR,Id_team))],className='')
     return country_flag
 
 def rows_countries_flags(IMAGES_DIR,Id_team,list_id_images):   
@@ -19,7 +19,7 @@ def rows_countries_flags(IMAGES_DIR,Id_team,list_id_images):
         dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[8]])],className='col'),
         dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[9]])],className='col'),
         dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[10]])],className='col'),
-        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[11]])],className='col')])
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[11]])],className='col')],className='row')
     return  rows_countries
 
 def rows_countries_flags_last(IMAGES_DIR,Id_team,list_id_images):   
@@ -32,7 +32,7 @@ def rows_countries_flags_last(IMAGES_DIR,Id_team,list_id_images):
         dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[5]])],className='col'),
         dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[6]])],className='col'),
         dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[7]])],className='col'),
-    ])
+    ],className='row')
     return  rows_countries
 
 def div_countries (IMAGES_DIR,Id_team,list_list_id_images):
