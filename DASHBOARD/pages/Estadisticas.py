c89355c63f662626727cc58d57f3a6ef8d428bae
aszurita <nzuritag@fiec.espol.edu.ec>
2023-06-02 21:23:41 -0500

    ok

--- DASHBOARD/pages/Estadisticas.py
+++ DASHBOARD/pages/Estadisticas.py
@@ -5,7 +5,7 @@ from dash import Input, Output, State, html
 import os
 import pandas as pd 
 import numpy as np
-import Functions
+from .Functions import div_countries
 nav_item = dbc.NavItem(dbc.NavLink("Link", href="#"))
 
 
@@ -78,9 +78,7 @@ navbar = dbc.Navbar(
 PAGES_DIR = os.path.dirname(__file__)
 APP_DIR = os.path.relpath(os.path.dirname(PAGES_DIR))
 ASSETS_DIR = os.path.relpath(os.path.join(APP_DIR,'assets'))
-print(ASSETS_DIR)
 DATAS_DIR = os.path.join(ASSETS_DIR,'datas')
-IMAGES_DIR = os.path.join(ASSETS_DIR,'Selecciones')
 #DATAS_DIR = '../assets/datas'
 IMAGES_DIR = '../assets/Selecciones'
 #print(IMAGES_DIR)
@@ -88,7 +86,7 @@ df_img_team = pd.read_csv(os.path.join(DATAS_DIR, "selecciones.csv"),sep=';')
 Id_team = df_img_team['team'].tolist()
 list_list_id_images = [[0,1,2,3,4,5,6,7,8,9,10,11],[12,13,14,15,16,17,18,19,20,21,22,23],[24,25,26,27,28,29,30,31]]
 countries = dbc.Container([
-    Functions.div_countries (IMAGES_DIR,Id_team,list_list_id_images)
+    div_countries (IMAGES_DIR,Id_team,list_list_id_images)
     ])
 
 #folder=r"C:/Users/Jonanyu 11.1/Desktop/img"
@@ -98,4 +96,4 @@ layout = html.Div(
     #html.Img(children=os.path.join(folder,"cm1.jpg"))
 
 )
-print(list_list_id_images[1])
+
--- DASHBOARD/pages/Functions.py
+++ DASHBOARD/pages/Functions.py
@@ -3,35 +3,36 @@ from dash import Dash, html
 import os 
 
 def country_flag(IMAGES_DIR,Id_team):
-    country_flag = dbc.Col([html.Img(src=os.path.join(IMAGES_DIR,Id_team))],md=1)
+    country_flag = dbc.Col([html.Img(src=os.path.join(IMAGES_DIR,Id_team))],className='col-lg-6 col-md-6 col-sm-6 col-xs-12')
     return country_flag
 
 def rows_countries_flags(IMAGES_DIR,Id_team,list_id_images):   
     rows_countries = dbc.Row([
-    country_flag(IMAGES_DIR,Id_team[list_id_images[0]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[1]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[2]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[3]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[4]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[5]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[6]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[7]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[8]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[9]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[10]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[11]])])
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[0]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[1]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[2]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[3]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[4]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[5]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[6]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[7]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[8]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[9]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[10]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[11]])],md=1)])
     return  rows_countries
 
 def rows_countries_flags_last(IMAGES_DIR,Id_team,list_id_images):   
     rows_countries = dbc.Row([
-    country_flag(IMAGES_DIR,Id_team[list_id_images[0]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[1]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[2]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[3]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[4]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[5]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[6]]),
-    country_flag(IMAGES_DIR,Id_team[list_id_images[7]])])
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[0]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[1]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[2]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[3]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[4]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[5]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[6]])],md=1),
+        dbc.Col([country_flag(IMAGES_DIR,Id_team[list_id_images[7]])],md=1),
+    ])
     return  rows_countries
 
 def div_countries (IMAGES_DIR,Id_team,list_list_id_images):
@@ -40,3 +41,4 @@ def div_countries (IMAGES_DIR,Id_team,list_list_id_images):
         rows_countries_flags(IMAGES_DIR,Id_team,list_list_id_images[1]),
         rows_countries_flags_last(IMAGES_DIR,Id_team,list_list_id_images[2])
     ])
+    return div_countries
