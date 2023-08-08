23568196ee62573d7ca1bb62b2d04e3ed57608f5
Luis <lromeror@fiec.espol.edu.ec>
2023-08-08 03:10:44 -0500

    CAmbioMadrugada

--- DASHBOARD/pages/Comparations.py
+++ DASHBOARD/pages/Comparations.py
@@ -95,36 +95,137 @@ PLAYER_DIR  = '../assets/Images'
 IMAGES_DIR = '../assets/Selecciones'
 
 Selections_Teams = dbc.Container([
-    dbc.Row(html.H2("CHOOSE TWO COUNTRIES"),className="Titlecenter"),
+    dbc.Row(html.H2("CHOOSE TWO COUNTRIES"), className="Titlecenter"),
     dbc.Row([
         dbc.Col([
-            dcc.Dropdown(list_teams,"Argentina",id='country1'),
-            ]),
+            dcc.Dropdown(options=list_teams, value="Argentina", id='country1'),
+        ]),
         dbc.Col([
-            dcc.Dropdown(list_teams,"Francia",id='country2')]
-                ),
-    ],className=""),
+            dcc.Dropdown(options=list_teams, value="Ecuador", id='country2'),
+        ]),
+    ]),
 ])
+
 Images_Teams = dbc.Container([
-    html.H1("hola"),
     dbc.Row(id="country"),
+])
+graficaEquipo1 = dbc.Container([
+    dbc.Row([
+        dcc.Graph(id='graficaCOuntry1')
     ])
+])
+graficaEquipo2 = dbc.Container([
+    dbc.Row([
+        dcc.Graph(id='graficaCOuntry2')
+    ])
+])
 
 @callback(
-    Output('ImgCountry1','children'),
-    [Input('country','value')],
-    [Input('country2','value')]
+    [Output('country', 'children'), Output('graficaCOuntry1', 'figure'),Output('graficaCOuntry2', 'figure')],
+    [Input('country1', 'value'), Input('country2', 'value')]
 )
-def getselectedteams(country1,country2): 
-    cod_img = df_img_team[df_img_team['seleccion']==country1]['team']
-    cod_img2 = df_img_team[df_img_team['seleccion']==country2]['team']
-    confe = df_img_team[df_img_team['seleccion']==country1]["continente"]
-    return dbc.Container([
-            dbc.Col(html.Img(src=os.path.join(IMAGES_DIR,cod_img))),
-            dbc.Col(html.Img(src=os.path.join(IMAGES_DIR,cod_img2))),
-            ])
+def get_selected_teams(country1, country2): 
+    global selected_country,selected_country2
+    selected_country = country1
+    selected_country2 = country2
+    
+    cod_img = df_img_team[df_img_team['seleccion'] == country1]['team'].values[0]
+    cod_img2 = df_img_team[df_img_team['seleccion'] == country2]['team'].values[0]
+    
+    img_style = {
+        'maxWidth': '100%',
+        'height': 'auto',
+        'borderRadius': '10px',
+        'margin': '0 auto',
+        'display': 'block',
+    }
+    image_col_style = {
+        'padding': '4px',
+    }
+    
+    return dbc.Row([
+        dbc.Col(html.Img(src=os.path.join(IMAGES_DIR, cod_img), style=img_style), width=6, className="my-2"),
+        dbc.Col(html.Img(src=os.path.join(IMAGES_DIR, cod_img2), style=img_style), width=6, className="my-2"),
+    ], justify="center"), graphTeams(selected_country),graphTeams(selected_country2)  # Llamamos a la función graphTeams con el país seleccionado
+
+df_gra= pd.read_csv('DASHBOARD/assets/datas/data_teamsCup.csv', sep=',')
+def graphTeams(country1):
+    df_graFil = df_gra[["team", "minutes", "goals", "avg_age", "possession"]]
+    trasnpuest = df_graFil.T
+    trasnpuest.columns = trasnpuest.iloc[0]
+    trasnpuest = trasnpuest.drop(trasnpuest.index[0])
+    dfNew = trasnpuest.reset_index()
+    dfNew = dfNew.rename(columns={'index': 'Category'})
+    fig = px.bar(dfNew, x='Category', y=country1, color='Category')
+    return fig
+
+Team_general=html.Div(
+            dbc.Container(
+                [
+                    html.Div(
+                        [
+                        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.H4('General graphics by team'),
+                        graficaEquipo1,
+                    ],
+                    width=6
+                ),
+                dbc.Col(
+                    [
+                    html.H4('General graphics by team'),
+                    graficaEquipo2,
+                    ],
+                    width=6
+                ),
+            ]
+        ),],
+            className="my-4",
+            style={"text-align": "center"},
+                    ),
+                ],
+                fluid=True,
+                className="container mt-4",
+                style={"background-color": "#f2f2f2", "padding": "20px","border-radius": "10px"},
+            ),
+        )
+
+
+graficaJugadoresDestacados=grafiProm=html.Div(
+            dbc.Container(
+                [
+                    html.Div(
+                        [
+                          dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.H4('Most Outstanding Players Charts'),
+                    ],
+                    width=6
+                ),
+                dbc.Col(
+                    [
+                        html.H4('Most Outstanding Players Charts'),
+                        
+                    ],
+                    width=6
+                ),
+            ]
+        ),],
+            className="my-4",
+            style={"text-align": "center"},
+                    ),
+                ],
+                fluid=True,
+                className="container mt-4",
+                style={"background-color": "#f2f2f2", "padding": "20px","border-radius": "10px"},
+            ),
+        )
 
 
 layout = html.Div(
-    [navbar,navbar2,Selections_Teams,Images_Teams],className="Principal"
+    [navbar,navbar2,Selections_Teams,Images_Teams, Team_general,graficaJugadoresDestacados],className="Principal"
 )
